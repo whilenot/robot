743e436ff00dfdb43d9d6fd0620e131aefa76014
whilenot <whilenot@users.noreply.github.com>
2016-05-10 09:56:04 +0200

    Taking the robot to next level

--- timers.cpp
+++ timers.cpp
@@ -7,10 +7,10 @@
 /* Initialization of Timer4 */
 void timer4_init(void)
 {
-  TIMSK4 = (1 << OCIE4A);  // Interrupt enable
+  TIMSK4 = (1 << OCIE4B);  // Interrupt enable
   TCCR4A = 0;
   TCCR4B = (1 << WGM42);  // CTC mode
-  OCR4A  = 62500;  // Total timer ticks
+  OCR4A  = 625;  // Total timer ticks
 
   TCCR4B |= (1 << CS42); // 256 prescaler
 }
@@ -26,3 +26,15 @@ void timer4_stop(void)
 {
   TCCR4B &= ~(1 << CS42);
 }
+
+/* Initialization of Timer4 */
+void timer3_init(void)
+{
+  TIMSK3 = (1 << OCIE3B);  // Interrupt enable
+  TCCR3A = 0;
+  TCCR3B = (1 << WGM32);  // CTC mode
+  OCR3A  = 625;  // Total timer ticks
+
+  TCCR3B |= (1 << CS32); // 256 prescaler
+}
+
